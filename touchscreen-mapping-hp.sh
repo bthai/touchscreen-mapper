ab3410a32b094d896537dde7a6ddc93ccd820192
bobby <bobby.thai@gmail.com>
2017-09-02 10:58:14 -0400

    update comment

--- touchscreen-mapping-hp.sh
+++ touchscreen-mapping-hp.sh
@@ -1,4 +1,4 @@
-# touchscreen mapping for dual touchscreens
+# touchscreen mapping for three touchscreens
 # more info at http://discourse.ubuntu.com/t/dual-touch-screen-monitors-working-on-14-04/1675
 # to find mapping
 xrandr
